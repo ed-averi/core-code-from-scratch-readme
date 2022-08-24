02b954926a4577359048849ebe61297dfc9959ad
Gerson Vasquez <averienglish@aol.com>
2022-08-23 22:13:53 -0600

    correction

--- extra katas/vowelcount.js
+++ extra katas/vowelcount.js
@@ -10,6 +10,7 @@ The input string will only consist of lower case letters and/or spaces.
 
 
 
+
 /**
  * We're taking a string, splitting it up into an array of characters, iterating through each
  * character, and checking if it's a vowel. If it is, we increment the vowel count.
@@ -23,7 +24,7 @@ function getCount(str) {
     const strArr= str.split("") //taking the spring and splitting up per character, individually
     
     strArr.forEach((el) => { //iterate each element of the string array for each el(element)
-      for (let i = 0; i < vowels.length - 1; i++){
+      for (let i = 0; i < vowels.length; i++){
         if (el === vowels[i]){
           vowelsCount++;
         }
